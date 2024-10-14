10a08b4b3e7c670cce71ec036c6ce61a9995bb6a
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-10-14 09:53:24 +0500

    backgroud color and formating code

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.purple,
         body: Center(
           child: Text("Hello and Welcome! to Flutter App Development"),
         ),
